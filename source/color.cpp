cfd1741a2fff948687e64bcf22bc9ef39899dc0c
Cora Langner <Cora@eduroam-055-064.scc.uni-weimar.de>
2018-05-28 16:28:32 +0200

    Aufgabe 3.14

---
+++ source/color.cpp
@@ -0,0 +1,27 @@
+#include "color.hpp"
+
+Color::Color() :
+    r{1.0},
+    g{1.0},
+    b{1.0} {}
+
+Color::Color(float c_c) : 
+    r{c_c},
+    g{c_c},
+    b{c_c} {}
+
+Color::Color(float c_r, float c_g, float c_b) :
+    r{c_r},
+    g{c_g},
+    b{c_b} {}
+
+
+    
+  
+
+
+
+
+
+
+
